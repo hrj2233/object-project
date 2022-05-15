ed22ec86a84c7a24f296062aaa7c5d2e7ad26a3e
hrj2233 <hrj2233@gmail.com>
2022-05-15 23:21:21 +0900

    complete object project

---
+++ problems/04-get-full-name.js
@@ -0,0 +1,26 @@
+/***********************************************************************
+Write a function `getFullName(person)` that takes in an person object
+and returns a string containing their full name.
+
+
+Examples:
+let p1 = {firstName: 'John', lastName: 'Doe'};
+getFullName(p1); // => 'John Doe'
+let p2 = {firstName: 'Charlie', lastName: 'Brown', age: 9};
+getFullName(p2); // => 'Charlie Brown'
+***********************************************************************/
+
+function getFullName(person) {
+  // your code here
+  let values = Object.values(person);
+  let str = [];
+  for (value of values) {
+    if (typeof value !== "number") {
+      str.push(value);
+    }
+  }
+  return str.join(" ");
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = getFullName;
